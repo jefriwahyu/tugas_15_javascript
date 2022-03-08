dd3548890dd2a680f13bef9e54b2d531e32caea3
Jefriwahyu <memberjefry1@gmail.com>
2022-03-08 10:44:39 +0700

    Tugas 15 Javascript
    Tugas ini saya kumpulkan untuk NIOMIC.

---
+++ index.js
@@ -0,0 +1,12 @@
+function ac(){
+  var ab = 8;
+  var bc = 6;
+
+
+  var ac = Math.sqrt(Math.pow(ab,2) + Math.pow(bc,2))
+  console.log("Nilai AB : " + ab)
+  console.log("Nilai BC : " + bc)
+  console.log("panjang sisi ac pada segitiga siku-siku tersebut adalah : " + ac + "cm")
+}
+
+ac()
